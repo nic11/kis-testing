d323f4af56d82a6c53f934ea2da14c0ba8de57f3
Roman Nikonov <niconneo@yandex.ru>
2021-05-02 13:21:09 +0300

    copy initial repo

---
+++ task4/tests/01-simple-library/AddTestCase.cpp
@@ -0,0 +1,7 @@
+//
+// Created by akhtyamovpavel on 5/1/20.
+//
+
+#include "AddTestCase.h"
+#include "Functions.h"
+
---
+++ task4/tests/01-simple-library/LeapTestCase.cpp
@@ -0,0 +1,9 @@
+//
+// Created by akhtyamovpavel on 5/1/20.
+//
+
+#include "LeapTestCase.h"
+
+#include <Functions.h>
+
+
